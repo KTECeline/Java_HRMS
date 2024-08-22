7edbcf8167bd13923b8f9bfe4a80948eb8132775
Khaw Tze Ern,Celine <157878271+KTECeline@users.noreply.github.com>
2024-08-22 20:40:21 +0800

    modify try branch

--- src/attendanceManagement/AttendanceManager.java
+++ src/attendanceManagement/AttendanceManager.java
@@ -15,6 +15,7 @@ public class AttendanceManager {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello WORLD");
     }
     
 }
