5e723719178d6da9b727ddb0568dae93b9df2e1d
huinannn <huinan26@gmail.com>
2024-08-22 22:59:42 +0800

    hi!

--- src/payrollManagement/PayrollManager.java
+++ src/payrollManagement/PayrollManager.java
@@ -15,6 +15,7 @@ public class PayrollManager {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Hello");
     }
     
 }
