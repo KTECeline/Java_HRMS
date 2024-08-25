cedd4b11a10bb4fac2154566b056ae95a4c10e1f
Khaw Tze Ern,Celine <157878271+KTECeline@users.noreply.github.com>
2024-08-25 20:58:06 +0800

    modify new files

---
+++ src/userManagement/DepartmentManager.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package userManagement;
+
+/**
+ *
+ * @author leopa
+ */
+public class DepartmentManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
